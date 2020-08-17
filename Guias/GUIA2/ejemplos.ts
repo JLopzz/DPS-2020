53b2107128a12b935c56d6f8625c68dc0e4e6e25
JLopzz <javylopzz@gmail.com>
2020-08-17 14:28:30 -0600

    variables

--- Guias/GUIA2/ejemplos.ts
+++ Guias/GUIA2/ejemplos.ts
@@ -1,2 +1,7 @@
-//ejemplo de escritura en consola
+//Imprimir en consola
 console.log("Hola Mundo, desde TypeScript");
+
+//Variables y Valores Primitivos
+var full_name:string = "Gerardo Lopez"
+var age:number = 22
+var developer:boolean = true
