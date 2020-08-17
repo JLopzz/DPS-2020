3ecf3ff4f985e28824617bcc54b1fa463d70ea9e
JLopzz <javylopzz@gmail.com>
2020-08-17 14:55:51 -0600

    'funciones'

--- Guias/GUIA2/ejemplos.ts
+++ Guias/GUIA2/ejemplos.ts
@@ -13,3 +13,28 @@ var arrNumeros:number[] = [123,234,345,456]
 //Enum
 enum ROLE{Employee, Manager, Admin, Developer}
 var role:ROLE = ROLE.Employee
+
+//Funciones
+/*funcion que no retorna nada ni reciba parametro*/
+function Hello():void{
+}
+
+/*fucnion que no retorne nada, y si recibe param*/
+function setNameEx1(name:string):void{
+}
+/*funcion que recibe dos parametros y devuelve un dato string*/
+function setNameEx2(name:string,surName:string):string {
+  return "string"
+}
+/** Ejemplo de variables internas en funciones **/
+function setNameEx3(name:string):string{
+let varInterna : string = "Javier"
+  return "Hola" + name
+}
+console.log("Hola"+varInterna)//retorna error de referencia, no existe en el contexto
+/** Ejemplo de variable global **/
+var varExterna = "Arely"
+function setNameEx4(name:string):string{
+  return "Hola"+name
+}
+console.log("Hola"+varExterna)
