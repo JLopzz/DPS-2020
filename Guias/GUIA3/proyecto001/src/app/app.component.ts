a3d8025266ac9b9011d791c0f787e6f483c10660
JLopzz <javylopzz@gmail.com>
2020-08-18 22:51:19 -0600

    Ejemplo 1 - Angular

--- Guias/GUIA3/proyecto001/src/app/app.component.ts
+++ Guias/GUIA3/proyecto001/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001';
+  nombre = 'Rodriguez Pablo'
+  edad = 40
+  email = 'rpablo@gmail.com'
+  sueldos = [ 1700, 1600, 1900 ]
+  activo = true
+
+  esActivo():string {
+    if(this.activo) return "Trabajador Activo"
+    else return "Trabajador Inactivo"
+  }
+  ultimos3Suelos(){
+    let suma = 0
+    for (let i = 0; i < this.sueldos.length; i++) {
+      suma += this.sueldos[i]
+    }
+    return suma
+  }
 }
