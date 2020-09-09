726255b66e9a641ddb87162482f0c5370155f5ba
JLopzz <javylopzz@gmail.com>
2020-09-09 11:01:35 -0600

    Discucion de resultados 1

--- Guias/GUIA4/crudAng/src/app/app.component.ts
+++ Guias/GUIA4/crudAng/src/app/app.component.ts
@@ -10,15 +10,18 @@ export class AppComponent {
   title = 'crudAng';
 
   alumnoArray : Alumno[] = [
-    {id: 1, name: "Alex",   lastname: "Campos",   age: 35},
-    {id: 2, name: "Maria",  lastname: "Lopez",    age: 20},
-    {id: 3, name: "Juan",   lastname: "Castro",   age: 25}
+    {id: 1, name: "Alex",   lastname: "Campos",   age: 35, dir : 'Col America', tel : '1234-5671', email : 'correo@dom.com'},
+    {id: 2, name: "Maria",  lastname: "Lopez",    age: 20, dir : 'Col La Palma', tel : '1234-5672', email : 'email@domin.net'},
+    {id: 3, name: "Juan",   lastname: "Castro",   age: 25, dir : 'Av las Moritas', tel : '1234-5673', email : 'otro.correo@dom.snn'}
   ]
   selectedAlumno : Alumno = {
     id: 0,
     name : '',
     lastname : '',
-    age : 0
+    age : null, 
+    dir : '', 
+    tel : '', 
+    email : ''
   }
 
   openForEdit( al : Alumno ) : void{ this.selectedAlumno = al }
@@ -28,13 +31,19 @@ export class AppComponent {
       this.selectedAlumno.id = this.alumnoArray.length + 1
       this.alumnoArray.push(this.selectedAlumno)
     }
-    this.selectedAlumno = { id: 0, name : '', lastname : '', age : 0 }
+    this.selectedAlumno = { id: 0, name : '', lastname : '', age : null, dir : '', tel : '', email : '' }
   }
 
   delete() : void{
     if(confirm('Esta seguro de elimniar el Registro?')){
       this.alumnoArray = this.alumnoArray.filter( x => x != this.selectedAlumno )
-      this.selectedAlumno = { id: 0, name : '', lastname : '', age : 0 }
+      this.selectedAlumno = { id: 0, name : '', lastname : '', age : null, dir : '', tel : '', email : '' }
     }
   }
+
+  cancel() : void{
+    this.selectedAlumno = { id: 0, name : '', lastname : '', age : null, dir : '', tel : '', email : '' }
+  }
+
+  print(obj:any){ console.log(obj)}
 }
